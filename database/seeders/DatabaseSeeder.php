0917d8605a9c1efa415c3d572c336bdd38c0b7d4
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-05 11:54:59 +0100

    added grade seeder to Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
             DepartmentProgramSeeder::class,
             CourseSeeder::class,
             SubjectSeeder::class,
+            GradeSeeder::class,
         ]);
     }
 }
