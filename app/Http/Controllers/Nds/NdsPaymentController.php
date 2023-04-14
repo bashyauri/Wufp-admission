705dbf053a2257d4874f8920ad0f360eef632cb2
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-14 12:05:57 +0100

    create Nds payment Controller

---
+++ app/Http/Controllers/Nds/NdsPaymentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Nds;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NdsPaymentController extends Controller
+{
+    public function invoice(){
+
+        return view('nds.invoice');
+    }
+}
