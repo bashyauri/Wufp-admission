2db77031d2aef2df08cd02f310eab4585b24c620
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-14 23:31:47 +0100

    update the services

--- config/services.php
+++ config/services.php
@@ -29,5 +29,11 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'remita'=> [
+        'ADMISSIONSERVICEID' =>  env('ADMISSIONSERVICEID'),
+        'CCESERVICEID' =>  env('CCESERVICEID'),
+        'MERCHANTID' =>  env('MERCHANTID'),
+        'APIKEY' =>  env('APIKEY'),
+    ]
 
 ];
