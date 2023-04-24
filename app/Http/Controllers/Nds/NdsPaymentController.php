94e0a9dae7693cc97c4586865139fca60e79acb9
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-04-24 14:49:22 +0100

    code refactoring

--- app/Http/Controllers/Nds/NdsPaymentController.php
+++ app/Http/Controllers/Nds/NdsPaymentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Nds;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
 use App\Services\Nds\PaymentService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -14,11 +15,21 @@ class NdsPaymentController extends Controller
     ) {}
     public function invoice(){
 
+        if ( $screeningDetail=$this->hasScreeningInvoice()){
+
+           return  redirect()->route('nds.screening.payment');
+        }
+
         $transactionId = $this->generateTransactionId();
         return view('nds.invoice')->with(['transactionId' => $transactionId]);
     }
+    public function makeScreeningPayment(){
+        $data = Payment::where(['student_id' =>auth()->user()->id,'resource' => 'Admission Payment'])->first();
 
+         return view('nds.payment')->with(json_decode($data,true));
+    }
     public function generateInvoice(Request $request) {
+
         $data = $request->all();
 
 
@@ -27,29 +38,25 @@ class NdsPaymentController extends Controller
         $data['apiHash'] = hash('sha512', $valuesToHash);
         try {
             $response = $this->ndsPaymentService->generateInvoice($data);
-
-            if(isset($response->RRR)){
                 $data['RRR'] = $response->RRR;
                 $data['statuscode'] = $response->statuscode;
                 $data['status'] = $response->status;
+                $this->ndsPaymentService->createPayment($data);
 
-                // $this->ndsPaymentService->createPayment($data);
-                return view('nds.payment')->with($data);
-            }
-
+               // return view('nds.payment')->with($data);
             return redirect()->route('nds.invoice')->with('success',$response->status );
 
         } catch (\Exception $ex) {
             Log::alert($ex->getMessage());
-            return redirect()->back()->withErrors(['msgError' => 'Something went wrong']);
+            return redirect()->back()->withErrors(['msgError' => 'Something went wrong:'.$response->status]);
         }
         // CRYPT_SHA512(config('services.remita.ADMISSIONSERVICEID').
         // config('services.remita.APIKEY'));
 
     }
-    private function checkInvoice()
+    private function hasScreeningInvoice():object | null
     {
-
+        return Payment::where(['student_id' =>auth()->user()->id,'resource' => 'Admission Payment'])->first();
     }
     private function generateTransactionId():string
     {
@@ -57,4 +64,5 @@ class NdsPaymentController extends Controller
         $tran=strtoupper($transcId);
         return "WUFPADM".$today =date("Ymd").$tran;
     }
+
 }
