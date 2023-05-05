bc097b1134aa8822f5d781506ed43f8d13662d96
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-05 19:03:49 +0100

    delete HOD admin

--- database/migrations/2023_04_30_193750_create_admins_table.php
+++ database/migrations/2023_04_30_193750_create_admins_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Department;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,11 +16,13 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Department::class);
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
             $table->string('phone')->unique();
+            $table->enum('role', ['superadmin', 'admin','hod']);
             $table->timestamps();
         });
     }
