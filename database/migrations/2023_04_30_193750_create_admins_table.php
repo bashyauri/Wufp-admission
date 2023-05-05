dceb4a98724e92de01dea1114c85fb8e5c9d6ced
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-05 18:59:17 +0100

    populate admin migrations

--- database/migrations/2023_04_30_193750_create_admins_table.php
+++ database/migrations/2023_04_30_193750_create_admins_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone')->unique();
             $table->timestamps();
         });
     }
