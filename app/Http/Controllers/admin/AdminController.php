bb4f5d65d548d529a008207cf1047cf20c0a3f12
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-05 19:48:18 +0100

    update AdminController

--- app/Http/Controllers/admin/AdminController.php
+++ app/Http/Controllers/admin/AdminController.php
@@ -3,9 +3,55 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::guard('admin')->attempt($credentials)) {
+            return redirect()->intended('/admin/dashboard');
+        }
+
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
+
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+
+        return redirect('/admin/login');
+    }
+
+    public function showRegistrationForm()
+    {
+        return view('admin.register');
+    }
+
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:admins'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        Admin::create($validatedData);
+
+        return redirect('/admin/login');
+    }
 }
