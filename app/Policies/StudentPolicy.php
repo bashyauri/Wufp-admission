8845fe591baf165cc6854a0c0f1b5a3fbed177bb
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-06 13:26:45 +0100

    minor update

---
+++ app/Policies/StudentPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Student;
+use App\Models\User;
+use App\Services\Nds\PaymentService;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class StudentPolicy
+{
+    use HandlesAuthorization;
+
+    use HandlesAuthorization;
+
+    public function printAdmissionForm(User $student)
+    {
+        return PaymentService::hasPaid("Admission Fees");
+    }
+
+
+}
