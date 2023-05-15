b90bb9655d0fbcba2e4c5f2088a141c90237d9b3
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-05-15 20:19:56 +0100

    update view

--- resources/views/nds/print-form.blade.php
+++ resources/views/nds/print-form.blade.php
@@ -6,9 +6,8 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
 <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css" rel="stylesheet" />
-<link href="../assets/css/form-wizard.css" rel="stylesheet" />
+<link  href="assets/css/form-wizard.css" rel="stylesheet" />
 <style type="text/css">
     body {
         /*padding: 2% 1% 2% 1%;
