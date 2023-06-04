7db9ab98d377a16db6ce644f9e7b307f73de3ec1
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-04 19:14:02 +0100

    code changes

--- resources/views/hnd/invoice.blade.php
+++ resources/views/hnd/invoice.blade.php
@@ -1,18 +1,19 @@
-@extends('user_type.auth', ['parentFolder' => 'hnd', 'childFolder' => 'none'])
+@extends('user_type.auth', ['parentFolder' => 'nds', 'childFolder' => 'none'])
 
 @section('content')
   <div class="row">
     <div class="col-lg-8 mx-auto">
       <div class="card mb-4">
+
         <div class="card-header p-3 pb-0">
           <div class="d-flex justify-content-between align-items-center">
             <div>
               <h6>Remita Details</h6>
               <p class="text-sm mb-0">
-                Remita no. <b>241342</b> from <b>23.02.2021</b>
+                Remita no. <b>{{'Wufpbk'}}</b> from <b>23.02.2021</b>
               </p>
               <p class="text-sm">
-                transaction Code: <b>KF332</b>
+                Amount: <b>N{{config('services.remita.ADMISSION_FEES')}}</b>
               </p>
             </div>
             <a href="javascript:;" class="btn bg-gradient-secondary ms-auto mb-0">Invoice</a>
@@ -27,20 +28,38 @@
                   <img src="https://images.unsplash.com/photo-1511499767150-a48a237f0083?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" class="avatar avatar-xxl me-3" alt="product image">
                 </div>
                 <div>
-                  <h6 class="text-lg mb-0 mt-2">HND Admission Fees</h6>
+                  <h6 class="text-lg mb-0 mt-2">NDS Admission Fees</h6>
                   <p class="text-sm mb-3">Order was delivered 2 days ago.</p>
                   <span class="badge badge-sm bg-gradient-success">Delivered</span>
                 </div>
               </div>
             </div>
             <div class="col-lg-6 col-md-6 col-12 my-auto text-end">
-              <a href="javascript:;" class="btn bg-gradient-info mb-0">Contact Us</a>
+              <a href="javascript:;" class="btn bg-gradient-info mb-0">Proceed To Remita</a>
               <p class="text-sm mt-2 mb-0">Do you like the product? Leave us a review <a href="javascript:;">here</a>.</p>
             </div>
           </div>
           <hr class="horizontal dark mt-4 mb-4">
           <div class="table-responsive p-0">
-            <form action="" method="POST">
+              @if($errors->any())
+            <div class="m-3  alert alert-warning alert-dismissible fade show" role="alert">
+                <span class="alert-text text-white">
+                {{$errors->first()}}</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    <i class="fa fa-close" aria-hidden="true"></i>
+                </button>
+            </div>
+            @endif
+            @if(session('success'))
+            <div class="m-3  alert alert-success alert-dismissible fade show" id="alert-success" role="alert">
+                <span class="alert-text text-white">
+                {{ session('success') }}</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    <i class="fa fa-close" aria-hidden="true"></i>
+                </button>
+            </div>
+            @endif
+            <form action="{{route('hnd.remita.invoice')}}" method="POST">
                 @csrf
                 <table class="table align-items-center justify-content-center mb-0">
                     <thead>
@@ -59,8 +78,8 @@
                             <td>
 
 
-                                <p class="text-sm font-weight-bold mb-0"> </p>
-                                <input id="transactionId" name="transactionId" value=""
+                                <p class="text-sm font-weight-bold mb-0">{{$transactionId}} </p>
+                                <input id="transactionId" name="transactionId" value="{{$transactionId}}"
                                     type="hidden" />
                             </td>
 
@@ -70,13 +89,13 @@
                                 <div class="d-flex px-2">
 
                                     <div class="my-auto">
-                                        <h6 class="mb-0 text-sm">Service</h6>
+                                        <h6 class="mb-0 text-sm">Service:</h6>
                                     </div>
                                 </div>
                             </td>
                             <td>
-                                <p class="text-sm font-weight-bold mb-0"></p>
-                                <input name="transactionId" value="" type="hidden" />
+                                <p class="text-sm font-weight-bold mb-0">{{config('services.remita.ADMISSIONSERVICEID')}}</p>
+                                <input name="service" value="{{config('services.remita.ADMISSIONSERVICEID')}}" type="hidden" />
                             </td>
 
                         </tr>
@@ -85,13 +104,13 @@
                                 <div class="d-flex px-2">
 
                                     <div class="my-auto">
-                                        <h6 class="mb-0 text-sm">Amount</h6>
+                                        <h6 class="mb-0 text-sm">Amount:</h6>
                                     </div>
                                 </div>
                             </td>
                             <td>
-                                <p class="text-sm font-weight-bold mb-0"></p>
-                                <input name="amount" value="" type="hidden" />
+                                <p class="text-sm font-weight-bold mb-0">N{{config('services.remita.ADMISSION_FEES')}}</p>
+                                <input name="amount" value="{{config('services.remita.ADMISSION_FEES')}}" type="hidden" />
                             </td>
 
                         </tr>
@@ -100,16 +119,16 @@
                                 <div class="d-flex px-2">
 
                                     <div class="my-auto">
-                                        <h6 class="mb-0 text-sm">Name</h6>
+                                        <h6 class="mb-0 text-sm">Name:</h6>
                                     </div>
                                 </div>
                             </td>
                             <td>
                                 <p class="text-sm font-weight-bold mb-0">
-
+                                    {{auth()->user()->last_name . ' ' . auth()->user()->first_name . ' ' . auth()->user()->middle_name}}
                                 </p>
                                 <input id="payerName" name="payerName"
-                                    value=""
+                                    value="{{auth()->user()->last_name . ' ' . auth()->user()->first_name . ' ' . auth()->user()->middle_name}}"
                                     type="hidden" />
                             </td>
 
@@ -124,8 +143,8 @@
                                 </div>
                             </td>
                             <td>
-                                <p class="text-sm font-weight-bold mb-0"> </p>
-                                <input id="payerEmail" name="payerEmail" value=""
+                                <p class="text-sm font-weight-bold mb-0">{{auth()->user()->email}} </p>
+                                <input id="payerEmail" name="payerEmail" value="{{auth()->user()->email}}"
                                     type="hidden" />
                             </td>
 
@@ -141,9 +160,9 @@
                                 </div>
                             </td>
                             <td>
-                                <p class="text-sm font-weight-bold mb-0"></p>
-                                <input name="payerPhone" value="" type="hidden" />
-                                <input name="paymenttype" value="RRRGEN" type="hidden" />
+                                <p class="text-sm font-weight-bold mb-0">{{auth()->user()->phone_no}}</p>
+                                <input name="payerPhone" value="{{auth()->user()->phone_no}}" type="hidden" />
+                                <input name="description" value="Admission Payment" type="hidden" />
 
                             </td>
 
@@ -151,33 +170,13 @@
                         </tr>
                         <tr>
                             <td>
-                                <input class="btn btn-success" type="submit" name="" value="Continue">
+                                <input class="btn btn-success" type="submit" name="" value="Proceed to remita">
                             </td>
                         </tr>
                     </tbody>
                 </table>
             </form>
-            <script type="text/javascript">
-                function PrintElem(elem) {
-                    //Popup($(elem).html());
-                    //var d = $('#' + elem).html();
-                    Popup($('#' + elem).html());
-                }
 
-                function Popup(data) {
-                    var mywindow = window.open('', 'new div', 'height=500,width=800');
-                    mywindow.document.write('<html><head><title></title>');
-                    //mywindow.document.write("<link rel=\"stylesheet\" href=\"style.css\" type=\"text/css\" media=\"all\"/>");
-                    mywindow.document.write('</head><body >');
-                    mywindow.document.write(data);
-                    mywindow.document.write('</body></html>');
-
-                    mywindow.print();
-                    mywindow.close();
-
-                    return true;
-                }
-            </script>
         </div>
         </div>
       </div>
