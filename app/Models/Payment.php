6136427a5dce0d306a3649c8b61fb49ac382c214
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-09 17:21:17 +0100

    modify relationship

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -12,6 +12,6 @@ class Payment extends Model
 
     public function user()
     {
-        return $this->hasMany(User::class, 'payment_id');
+        return $this->belongsTo(User::class, 'payment_id');
     }
 }
