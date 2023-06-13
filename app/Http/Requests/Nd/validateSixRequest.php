0d157c3999ed33cfad37ec61752943867ff689a7
bashyauri <80903696+bashyauri@users.noreply.github.com>
2023-06-13 13:13:44 +0100

    add validate six

---
+++ app/Http/Requests/Nd/validateSixRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests\Nd;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class validateSixRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'jamb_no' => ['required'],
+            'file' => [Rule::requiredIf(!isset(auth()->user()->jambDetails->file)),'image','mimes:jpg,png'],
+            'score' => ['required']
+        ];
+    }
+}
